9a293f8c60970a160c47e2c5f58f2a3d0154bf11
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-24 14:57:10 +0200

    feat: Add validation for amount and type

---
+++ fraud-sight/backend/api/middlewares/validateTransaction.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+// validation
+export const validateTransaction = (req: Request, res: Response, next: NextFunction) => {
+  const { amount, type } = req.body;
+  if(!amount || !type) {
+    return res.status(400).json({ message: 'amount and type are required'})
+  }
+  next();
+};
