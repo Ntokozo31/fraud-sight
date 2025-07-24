4bb776011c842e04a90f1d023d207fe8965aadc3
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-24 14:44:14 +0200

    feat: router to get transaction by Id
    - Import validate middleware
    - Add router that will get transaction by it Id
    - Add router that will update transaction by it Id
    - Add delete router to permanantly delete transaction

--- fraud-sight/backend/api/routes/transactionRoutes.ts
+++ fraud-sight/backend/api/routes/transactionRoutes.ts
@@ -1,15 +1,33 @@
 import { Router } from "express";
 
 // Import controller function
-import { getAllTransactions, createTransaction } from "../controllers/transactionController";
+import {
+  getAllTransactions,
+  createTransaction,
+  getTransactionById,
+  updateTransaction,
+  deleteTransaction } from "../controllers/transactionController";
+
+// Import validation middleware
+import { validateTransaction } from "../middlewares/validateTransaction";
 
 // Router instance
 const router = Router();
 
 // Route for transactions
-router.get('/', getAllTransactions) 
+router.get('/', getAllTransactions);
 
-router.post('/', createTransaction)
+// Router to create transaction
+router.post('/', validateTransaction, createTransaction);
+
+// Router to get transaction by it Id
+router.get('/:id', getTransactionById);
+
+// Router to update transaction by it id
+router.put('/:id', validateTransaction, updateTransaction);
+
+// Router to ddelete transaction by it id
+router.delete('/:id', deleteTransaction)
 
 // Export router
 export default router;
