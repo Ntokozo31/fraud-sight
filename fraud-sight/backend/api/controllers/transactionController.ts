b674f2f8c5bb5404b2cd3fbc1465ed16fd4f82ca
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-27 14:40:04 +0200

    refact: moves database interaction
    - Import transactionService
    - Moves database interactions and it methods to transactionServices file
    - Controller functions will now deal with http only

--- fraud-sight/backend/api/controllers/transactionController.ts
+++ fraud-sight/backend/api/controllers/transactionController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 
-import { prisma } from '../utils/prismaClient';
-import { request } from 'http';
+// Import transaction service
+import * as transactionService from '../services/transactionService'
+
 
 // Get all transactions controller function
 export const getAllTransactions = async (req: Request, res: Response) => {
   try {
-    const transactions = await prisma.transaction.findMany();
+    const transactions = await transactionService.getAllTransactions();
     res.status(200).json(transactions)
-  } catch (error) {
+  } catch (_error) {
     res.status(500).json({ message: 'Failed to fetch transactions'})
   }
 }
@@ -18,12 +19,9 @@ export const getAllTransactions = async (req: Request, res: Response) => {
 export const createTransaction = async (req: Request, res: Response) => {
   try {
     const { amount, type } = req.body;
-    if (!amount || !type) {
-      return res.status(400).json({ message: 'amount and type are required' })
-    }
-    const transaction = await prisma.transaction.create({ data: { amount, type } })
+    const transaction = await transactionService.createTransaction({ amount, type })
     res.status(201).json(transaction)
-  } catch (error) {
+  } catch (_error) {
     res.status(500).json({ message: 'Failed to create transaction' })
   }
 }
@@ -31,47 +29,44 @@ export const createTransaction = async (req: Request, res: Response) => {
 // Get transaction by it own id
 export const getTransactionById = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params
-    const transaction = await prisma.transaction.findUnique({ where: { id: Number(id)}})
+    const id  = Number(req.params.id);
+    const transaction = await transactionService.getTransactionById(id);
     if (!transaction) {
-      return res.status(404).json({ message: 'No transaction found'})
+      return res.status(404).json({ message: 'No transaction found' });
     }
-    res.status(200).json({ message: 'Your transaction', transaction})
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch transaction' })
+    res.status(200).json({ message: 'Your transaction', transaction });
+  } catch (_error) {
+    res.status(500).json({ message: 'Failed to fetch transaction' });
   }
 };
 
-// Update transcation by it Id controller function 
+// Update transaction by its ID controller function
 export const updateTransaction = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params;
+    const id = Number(req.params.id);
     const { amount, type } = req.body;
 
     // Check if transaction exist
-    const existing = await prisma.transaction.findUnique({ where: { id: Number(id) } })
+    const existing = await transactionService.getTransactionById(id)
     if (!existing) {
       return res.status(404).json({ message: 'No transaction found' })
     }
 
-    const transaction = await prisma.transaction.update({
-      where: { id: Number(id)},
-      data: { amount: Number(amount), type }
-    });
-    res.status(200).json({ message: 'Successufully updated', transaction})
-    } catch (error) {
+    const transaction = await transactionService.updateTransaction(id, { amount, type })
+    res.status(200).json({ message: 'Successfully updated', transaction })
+    } catch (_error) {
       res.status(500).json({ message: 'Failed to update transaction'})
     }
-}
+};
 
 
 // Delete transaction by it Id controller function
 export const deleteTransaction = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params;
-    await prisma.transaction.delete({ where: {id: Number(id) } });
+    const id = Number(req.params.id);
+    await transactionService.deleteTransaction(id);
     res.status(200).json({ message: 'Successfully deleted'})
-  } catch (error) {
+  } catch (_error) {
     res.status(500).json({ message: 'Failed to delete transaction'})
   }
 }
