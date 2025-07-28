b9986f5cd37e1daca350aa6b40a72d36d83ca5a5
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-28 13:44:00 +0200

    feat: log interaction
    - Import transactionService, and log
    - Initialize cacheKey
    - Add cache from redis
    - Log message
    - Use del to make sure redis stays up to date when db has new changes

--- fraud-sight/backend/api/controllers/transactionController.ts
+++ fraud-sight/backend/api/controllers/transactionController.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express';
 
 // Import transaction service
-import * as transactionService from '../services/transactionService'
+import * as transactionService from '../services/transactionService';
 
+// Import redis client
+import { redis } from '../../redis/redisClient';
+
+// Import logger utility
+import { log } from '../utils/logger';
 
 // Get all transactions controller function
 export const getAllTransactions = async (req: Request, res: Response) => {
+  const cacheKey = 'transactions: allTransactions';
   try {
+    const cached = await redis.get(cacheKey);
+    if (cached) {
+      log('Cache hit for transaction');
+      return res.status(200).json(JSON.parse(cached));
+    }
+
     const transactions = await transactionService.getAllTransactions();
-    res.status(200).json(transactions)
+    log('Cache miss for transactions');
+    await redis.set(cacheKey, JSON.stringify(transactions), { EX: 60 });
+    log('Transactions catched in Redis');
+    res.status(200).json({ message: 'Transactions', transactions })
   } catch (_error) {
+    log('Error fetching transactions')
     res.status(500).json({ message: 'Failed to fetch transactions'})
   }
 }
@@ -20,6 +36,8 @@ export const createTransaction = async (req: Request, res: Response) => {
   try {
     const { amount, type } = req.body;
     const transaction = await transactionService.createTransaction({ amount, type })
+    await redis.del('transactions: allTransactions');
+    log('Transaction created: ' + JSON.stringify(transaction));
     res.status(201).json(transaction)
   } catch (_error) {
     res.status(500).json({ message: 'Failed to create transaction' })
@@ -31,6 +49,8 @@ export const getTransactionById = async (req: Request, res: Response) => {
   try {
     const id  = Number(req.params.id);
     const transaction = await transactionService.getTransactionById(id);
+    await redis.del('transactions: allTransactions');
+    log('Transaction fetched: ' + JSON.stringify(transaction))
     if (!transaction) {
       return res.status(404).json({ message: 'No transaction found' });
     }
@@ -46,13 +66,9 @@ export const updateTransaction = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     const { amount, type } = req.body;
 
-    // Check if transaction exist
-    const existing = await transactionService.getTransactionById(id)
-    if (!existing) {
-      return res.status(404).json({ message: 'No transaction found' })
-    }
-
     const transaction = await transactionService.updateTransaction(id, { amount, type })
+    await redis.del('transactions: allTransactions')
+    log('Transaction successfully updated')
     res.status(200).json({ message: 'Successfully updated', transaction })
     } catch (_error) {
       res.status(500).json({ message: 'Failed to update transaction'})
@@ -65,6 +81,8 @@ export const deleteTransaction = async (req: Request, res: Response) => {
   try {
     const id = Number(req.params.id);
     await transactionService.deleteTransaction(id);
+    await redis.del('transactions: allTransactions');
+    log('Transaction succesfully deleted')
     res.status(200).json({ message: 'Successfully deleted'})
   } catch (_error) {
     res.status(500).json({ message: 'Failed to delete transaction'})
