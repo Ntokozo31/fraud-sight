4101b1857f16f6656bc31ed46cc62a5812523320
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-28 13:53:50 +0200

    feat: Check if transaction exist middleware

--- fraud-sight/backend/api/middlewares/validateTransaction.ts
+++ fraud-sight/backend/api/middlewares/validateTransaction.ts
@@ -1,11 +1,24 @@
 // Import Request, Response, and NextFunction from express
 import { Request, Response, NextFunction } from 'express';
 
+// Import transactionService
+import * as transactionService from '../services/transactionService';
+
 // validation
 export const validateTransaction = (req: Request, res: Response, next: NextFunction) => {
   const { amount, type } = req.body;
   if(!amount || !type) {
     return res.status(400).json({ message: 'amount and type are required'})
+  }
+  next();
+};
+
+// Check if Transacttion exist
+export const checkTransactionExists = async (req: Request, res: Response, next: NextFunction) => {
+  const id = Number(req.params.id);
+  const transaction = await transactionService.getTransactionById(id);
+  if (!transaction) {
+    return res.status(404).json({ message: 'No transaction found' });
   }
   next();
 };
