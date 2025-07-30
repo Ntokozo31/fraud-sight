e215a6bf35d8753fc786cd812e2181c153212b41
Ntokozo31 <dubentokozo45@yahoo.com>
2025-07-30 11:31:40 +0200

    feat: Import and initialize dotenv from dotenv

--- fraud-sight/backend/api/utils/prismaClient.ts
+++ fraud-sight/backend/api/utils/prismaClient.ts
@@ -1,6 +1,13 @@
 // Import the PrismaClient from the Prisma package
 import { PrismaClient }  from '../../generated/prisma';
 
+// Import the dotenv package to load environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Get the database URL from environment variables
+const dbUrl = process.env.DATABASE_URL;
+
 // Initialize prisma
 // This creates a new instance of the PrismaClient
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ datasources: { db: { url: dbUrl } } });
