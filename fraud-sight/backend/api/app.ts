4d7e2eeb8c4bb9804804b6a78c3e0cf558054398
Ntokozo31 <dubentokozo45@yahoo.com>
2025-08-07 11:42:29 +0200

    feat: Rate limit middleware
    - Add rate limiting middleware for requesting api
    - Add hpp middleware
    - Add helmet middleware
    - Add health router
    - Add corse middleware

--- fraud-sight/backend/api/app.ts
+++ fraud-sight/backend/api/app.ts
@@ -1,7 +1,27 @@
 import express, { Application } from 'express';
 
+//import { Request, Response } from 'express';
+
+// Import cors
+import cors from 'cors';
+
+// Import helmet
+import helmet from 'helmet';
+
+// Import hpp
+import hpp from 'hpp';
+
+// Import rateLimit
+import rateLimit from 'express-rate-limit';
+
 // Import router
 import transactionsRouter from './routes/transactionRoutes';
+
+// Import swaggerUi
+import swaggerUi from 'swagger-ui-express';
+
+// import swaggerDocument
+import swaggerDocument  from '../../docs/swagger.json';
 
 // Import error handler middleware
 import { errorHandler } from './middlewares/errorHandler';
@@ -18,11 +38,56 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Transcation router
+// HPP middleware
+app.use(hpp());
+
+// Security headers
+app.use(helmet());
+
+// Helmet middleware
+app.use(helmet.hsts({
+  maxAge: 31536000,
+  includeSubDomains: true,
+  preload: true
+}));
+
+// Helmet frameguard middleware
+app.use(helmet.frameguard({ action: 'deny' }));
+
+
+app.use((req, res, next) => {
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+});
+
+// Cors middleware
+app.use(cors({
+  origin: ['', ''],
+  credentials: true
+}));
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many request from this IP Adress, please try again later'
+});
+
+// Limit requests 
+app.use(limiter);
+
+// Transaction router
 app.use('/api/transactions', transactionsRouter);
 
+// Health Router
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok', timeStamp: new Date().toISOString() })
+});
 // Error handler middleware
 app.use(errorHandler);
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Start the server
 if(require.main === module) {
@@ -31,4 +96,5 @@ if(require.main === module) {
   });
 }
 
+// Export app
 export default app;
