765f23c64752a5971a03d06b3ba6843a7cd1dba0
Ntokozo31 <dubentokozo45@yahoo.com>
2025-08-14 12:24:30 +0200

    feat: user routes
    - Add router to get user profile using get method
    - Add router to update user profile using put method
    - Add router to change user password using post method
    - Add router to get user statistics using get method
    - Add router to get users, only admin can perfom that using get method
    - Add router to get single user, only admin can perfom that using get method
    - Add routeer to update user profile with only admin using put method
    - Add router to get user statistics with only admin can perfom that using get method

--- fraud-sight/backend/api/routes/userRoutes.ts
+++ fraud-sight/backend/api/routes/userRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-
-// Import createUser controller function
 import { createUser, userLogin} from "../controllers/userController";
+import * as userController from '../controllers/userController';
+import { authMiddleware } from '../middlewares/auth';
+import { requireAdmin } from '../middlewares/rbac';
+import { getUserStatistics } from '../services/userServices';
+
 
 // Router instance
 const router = Router();
@@ -9,9 +12,32 @@ const router = Router();
 // New user router
 router.post('/signup', createUser);
 
-// Login user router
+// Router login
 router.post('/login', userLogin);
 
+// Router get profile
+router.get('/profile', authMiddleware, userController.getUserProfile);
+
+// Router update profile
+router.put('/profile', authMiddleware, userController.updateUserProfile);
+
+// Router change password
+router.post('/change-password', authMiddleware, userController.changeUserPassword);
+
+// Router get statistics
+router.get('/statistics', authMiddleware, userController.getUserStatistics);
+
+// Router get users: Admin
+router.get('/admin/users', authMiddleware, requireAdmin, userController.getAllUsers);
+
+// Router get user: Admin
+router.get('/admin/user/:userId', authMiddleware, requireAdmin, userController.getAnyUserProfile);
+
+// Router update user: Admin
+router.put('/admin/user/:userId', authMiddleware, requireAdmin, userController.adminUpdateUser);
+
+// Router get user statistics
+router.get('/admin/user/:userId/statistics', authMiddleware, requireAdmin, userController.getAnyUserStatistics)
 
 // Export router
 export default router
